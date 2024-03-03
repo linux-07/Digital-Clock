308b1ba98825b8d2ef9733441c55d02449fa355f
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-03 19:28:50 +0530

    Update date and day too in real time

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 let newdate = new Date();
 
-// Creating Day
-const day = newdate.getDay();
-const datearr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-// Inserting Day
-document.querySelector(".day").innerHTML = datearr[day];
-
-// Creating Date
-const date = newdate.getDate();
-const month = newdate.getMonth();
-const year = newdate.getFullYear();
-
-// Formatting Date
-const formattedDate = date < 10 ? '0' + date : date;
-const formattedMonth = month < 10 ? '0' + month : month;
-const fullDate = `${formattedDate}/${formattedMonth}/${year}`
-
-// Inserting Date
-document.querySelector(".date").innerHTML = fullDate;
-
-
-// Inserting and Updating Time every second
 setInterval(() => {
     newdate = new Date();
+
+    // Creating Day
+    const day = newdate.getDay();
+    const datearr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    // Inserting Day
+    document.querySelector(".day").innerHTML = datearr[day];
+
+    // Creating Date
+    const date = newdate.getDate();
+    const month = newdate.getMonth();
+    const year = newdate.getFullYear();
+
+    // Formatting Date
+    const formattedDate = date < 10 ? '0' + date : date;
+    const formattedMonth = month < 10 ? '0' + month : month;
+    const fullDate = `${formattedDate}/${formattedMonth}/${year}`
+
+    // Inserting Date
+    document.querySelector(".date").innerHTML = fullDate;
+
+    // Updating and inserting time
     const hour = newdate.getHours();
     const min = newdate.getMinutes();
     const secs = newdate.getSeconds();
